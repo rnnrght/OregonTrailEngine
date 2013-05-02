60b6913f5580864e48956190affb9a17f6c8674e
zphricz <zphricz@vt.edu>
2013-05-01 22:40:55 -0400

    Added to game logic somewhat

--- Game.py
+++ Game.py
@@ -3,48 +3,102 @@
 from GameObjects import *
 import os
 
+##### VARIOUS FUNCTIONS #####
+
 def clearScreen():
     os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
 
+def displayStatus():
+    clearScreen()
+    global currentCity
+    global distanceUnit
+    global Supplies
+    global diseaseName
+    city = Cities[currentCity]
 
-##### GAME VARIABLES #####
+    print "Heading to: " +  city.name
+    print "Distance: " +  str(city.distanceTo) + " " + distanceUnit + "\n"
+    print "Pace of travel: " + pace
+    print "Food per meal: " + meal + "\n"
 
-startScreen = (
-"                    DYSENTERY TRAIL                    \n"
-"In the year 20XX, the world succumbed to its poor diet.\n"
-"   Will you and your group of survivors last in their  \n"
-"                  journey for a cure?                  \n" )
+    for supply in Supplies:
+        print supply.name + ": " + str(supply.amount)
 
+    print ""
 
-baseTravelRate = 100
-travelRateFactor = 1.0
+    for character in Characters:
+        print character.name + ": "
+        print "- Health: " + str(character.health)
+        if character.isSick:
+            print "- Has " + diseaseName
+        print ""
 
-baseEatRate = 50
-eatRateFactor = 1.0
+def travelLoop():
+    global currentCity
+    global Supplies
+    global baseEatRate
+    global baseTravelRate
+    global pace
+    global meal
+    city = Cities[currentCity]
+    while city.distanceTo > 0:
+        displayStatus()
+        if pace == "normal":
+            travelRateFactor = 1.0
+        elif pace == "fast":
+            travelRateFactor = 1.25
+        elif pace == "grueling":
+            travelRateFactor = 1.5
 
-hoursToScavenge = 24
+        if meal == "normal":
+            eatRateFactor = 1.0
+        elif meal == "small":
+            eatRateFactor = .75
+        elif meal == "skimpy":
+            eatRateFactor = 0.5
 
-numCharacters = 5
+        city.distanceTo -= baseTravelRate * travelRateFactor
+        Supplies[0].amount -= baseEatRate * eatRateFactor * len(Characters)
+        choice = raw_input("1. Continue\n2. Travel Options\n")
+        if choice == "2":
+            travelOptions()
+    currentCity += 1
 
-nameOfObstacle = "Dysentery"
-obstacleStatus = "Calm"
-# Obstacle to pass (I.E. the river, or zambies)
-# Status holds the difficulty of passing
-
-##########################
-
+def travelOptions():
+    clearScreen()
+    global pace
+    global meal
+    print "Travel rate:"
+    print "1. Normal pace"
+    print "2. Fast pace"
+    print "3. Grueling pace\n"
+    print "Meal amount:"
+    print "4. Normal meal"
+    print "5. Small meal"
+    print "6. Skimpy meal\n"
+    print "7. Done"
+    choice = input("What would you like to do?: ")
+    if choice == 1:
+        pace = "normal"
+    elif choice == 2:
+        pace = "fast"
+    elif choice == 3:
+        pace = "grueling"
+    elif choice == 4:
+        meal = "normal"
+    elif choice == 5:
+        meal = "small"
+    elif choice == 6:
+        meal = "skimpy"
+    elif choice == 7:
+        return
+    travelOptions()
 
 Characters = []
 # Characters[0] = Main character
 
 # Each supply is constructed with its name, followed by the number that can be scavenged per hour
 Supplies = []
-Supplies.append(Supply("Food", 100))
-Supplies.append(Supply("Ammunition", 100))
-Supplies.append(Supply("Spare Parts", 100))
-Supplies.append(Supply("Fuel", 100))
-Supplies.append(Supply("Money", 100))
-Supplies.append(Supply("Medicine", 100))
 # Each supply has a name, and the number of remaining units
 
 # Supplies[0] = Food
@@ -55,12 +109,67 @@ Supplies.append(Supply("Medicine", 100))
 # Supplies[5] = Meds
 
 Cities = []
-
 # Each city has a name, distance to next city
 
 Events = []
 
 # Each Event has a name, probibility, and factors that effect the game
+
+
+##### VARIABLES TO PARSE #####
+
+startScreen = (
+"                    DYSENTERY TRAIL                    \n"
+"In the year 20XX, the world succumbed to its poor diet.\n"
+"   Will you and your group of survivors last in their  \n"
+"                  journey for a cure?                  \n" )
+
+
+# Units that are travelled per travel cycle
+baseTravelRate = 100
+pace = "normal" # NOT PARSEABLE
+
+# Units eaten per travel cycle
+baseEatRate = 20
+meal = "normal" # NOT PARSEABLE
+
+hoursToScavenge = 24
+
+numCharacters = 3
+
+currentCity = 0
+
+diseaseName = "dysentery"
+
+distanceUnit = "miles"
+
+# All of the supply types: first arg is name, second is number gatherable per hour
+Supplies.append(Supply("Food", 100))
+Supplies.append(Supply("Ammunition", 100))
+Supplies.append(Supply("Spare Parts", 100))
+Supplies.append(Supply("Fuel", 100))
+Supplies.append(Supply("Money", 100))
+Supplies.append(Supply("Medicine", 100))
+
+# All of the cities: first arg is name, second is number of distance units to it
+Cities.append(City("Tokyo", 350.0))
+Cities.append(City("New York", 350.0))
+Cities.append(City("Toronto", 450.0))
+Cities.append(City("Vienna", 400.0))
+Cities.append(City("Richmond", 250.0))
+Cities.append(City("San Fransisco", 500.0))
+Cities.append(City("Tenochtitlan", 450.0))
+Cities.append(City("Redmond", 400.0))
+Cities.append(City("Troy", 150.0))
+Cities.append(City("Gary", 550.0))
+
+nameOfObstacle = "Dysentery"
+obstacleStatus = "Calm"
+# Obstacle to pass (I.E. the river, or zambies)
+# Status holds the difficulty of passing
+
+##########################
+
 
 ##### GAME EXECUTION STARTS #####
 
@@ -115,3 +224,9 @@ while True:
 
 # Begin Adventure
 
+while currentCity < len(Cities):
+    travelLoop()
+
+clearScreen()
+
+print "GAME OVER"
--- GameObjects.py
+++ GameObjects.py
@@ -3,10 +3,19 @@ class Character:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.sick = False
+        self.isSick = False
 
 class Supply:
     def __init__(self, name, rate):
         self.name = name
         self.rate = rate
         self.amount = 0
+
+class City:
+    def __init__(self, name, distanceTo):
+        self.name = name
+        self.distanceTo = distanceTo
+
+class Event:
+    def __init__(self, name):
+        self.name = name

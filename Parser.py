ccdb8e50031217e6664ae0ed3b78bc4f04ae6090
Mark Koninckx <koninckx@vt.edu>
2013-05-01 23:46:34 -0400

    Expand parser self-test

--- Parser.py
+++ Parser.py
@@ -22,5 +22,17 @@ class Parser:
 			return ""
 
 if __name__ == '__main__' : #run this file directly for a quick self-test
+	print "Parser self-test:"
+	print "- constructing Parser object:"
 	parser = Parser()
+
+	print "- printing attributes dictionary:"
 	print parser.allAttributes()
+
+	print "\n\n"
+	print "- printing results of get(\"ammoRescName\")"
+	print parser.get("ammoRescName")
+	print "- printing results of get(\"garbage\")"
+	print parser.get("garbage")
+	print "- printing results of get(\"fuelScavRate\")"
+	print parser.get("fuelScavRate")

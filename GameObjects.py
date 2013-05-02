a6ebf1f29ef762b27ffb4c401d2d2bba2e0b4ed6
Robert Garland <rgarland@vt.edu>
2013-05-02 16:17:59 -0400

    Add multiple-resource effects.

--- GameObjects.py
+++ GameObjects.py
@@ -16,17 +16,11 @@ class City:
         self.name = name
         self.distanceTo = distanceTo
 
-class Effect:
-    def __init__(self, attribute, amount, message):
-	self.attribute = attribute
-	self.amount = amount
-	self.message = message
-
 class Event:
-    def __init__(self, name, description, options, chances, goodEffects, badEffects):
-        self.name = name #same as key
-	self.description = description #event message
-	self.options = options#array of options
-	self.chances = chances#array of % chance of sucess
-	self.goodEffects = goodEffects #array of good effects if success
-	self.badEffects = badEffects #array of bad effects if fail
+    def __init__(self, name):
+        self.name = name
+	self.description = description
+	self.options = options
+	self.chances = chances
+	self.goodEffects = goodEffects 
+	self.badEffects = badEffects

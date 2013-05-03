dbe4a9e5e1512d66108850f45b55040771f09b48
Robert Garland <rgarland@vt.edu>
2013-05-03 05:34:36 -0400

    Added city events

--- Game.py
+++ Game.py
@@ -419,7 +419,10 @@ while running and currentCity < len(Cities):
 
     # City event
     if currentCity != len(Cities):
-        pass
+        clearScreen()
+        raw_input("You have arrived at "+Cities[currentCity-1].name+".")
+        event = datfiles.getTypeEvent("C")
+        handleEvent(event)
 
 clearScreen()
 
